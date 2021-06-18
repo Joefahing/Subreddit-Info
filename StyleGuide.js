91c3541e2ec79fa7e7d5a86e40e45a82b96b88e0
joefahing <cao11d@uwindsor.ca>
2021-06-18 13:27:12 -0400

    first commit

---
+++ StyleGuide.js
@@ -0,0 +1,21 @@
+const Style = {
+    color:{
+        primary:{
+            brand: '#ff8c5f',
+            cta: '#62e6ac',
+            white: '#fff',
+            black: '#191919',
+            grayscale300: '#e3e3e3',
+            grayscale400: '#d2d2d2',
+            grayscale600: '#a3a3a3',
+            grayscale800: '#7d7d7d',
+            link: '#1755b7'
+        }
+    },
+
+    font:{
+        main_font: 'https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600;700&display=swap',
+        alternative_font: 'Monaco'
+    }
+
+}
